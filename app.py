52204c3aaf0754af7ff7a5c72a80a4c449ffeb16
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-11-06 20:52:23 +0530

    create front end

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain_community.llms import Ollama  
 from langchain.vectorstores import Chroma 
 from langchain.memory import ConversationBufferMemory
+from langchain.schema import AIMessage,HumanMessage
 
 app=Flask(__name__)
 
@@ -22,26 +23,54 @@ vector_data=Chroma(
     embedding_function=embeddings
 )
 
-retrival=vector_data.as_retriever(search_type="similarity",search_kwargs={"k":3})
+#Create retriever
+retriever=vector_data.as_retriever(search_type="similarity",search_kwargs={"k":3})
 
 llm=Ollama(model="gemma3") 
 
+
+#Add conversational memory
 memory=ConversationBufferMemory(memory_key="chat_history",return_messages=True)
+
+# Build chain with memory-aware prompt
 question_answer_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
-rag_chain=create_retrieval_chain(retrival,question_answer_chain)
+rag_chain=create_retrieval_chain(retriever,question_answer_chain)
 
 
 @app.route("/")
 def index(): 
     return render_template("index.html")
 
+
 @app.route("/get",methods=["GET","POST"])
 def chat(): 
     msg=request.form["msg"]
-    print("input:",msg)
-    response=rag_chain.invoke({"input":msg})
-    print("Response:",response["answer"])
-    return str(response["answer"])
+    print("USER INPUT:",msg)
+
+    chat_hist_str="\n".join([
+        f"user:{m.content}" if isinstance(m,HumanMessage) else f"bot:{m.content}"
+
+        for m in memory.chat_memory.messages
+    ])
+
+    response=rag_chain.invoke({
+        "input":msg,
+        "chat_history":chat_hist_str
+    })
+
+    answer=response["answer"]
+
+    print("RESPONSE:",answer)
+
+    memory.chat_memory.add_message(HumanMessage(content=msg))
+    memory.chat_memory.add_ai_message(AIMessage(content=answer))
+
+    return str(answer)
+
+@app.route("/reset")
+def reset_memory(): 
+    memory.clear()
+    return "clear chat memory"
 
 
 if __name__ == "__main__": 
